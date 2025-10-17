af79e1ef131e165f840e154cf3929bf2cd33c1a4
jordangrace31 <156056732+jordangrace31@users.noreply.github.com>
2025-10-17 08:21:13 +0200

    Move assets to public directory

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/sandbox-game-jojo/',
+  publicDir: 'public',  // Public assets folder
+  build: {
+    assetsInlineLimit: 0,  // Don't inline assets, keep them as separate files
+  }
 });
